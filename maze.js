7009a942ecc217d4d524eab5d057fb6b7a741c44
leesjensen <npicker@hotmail.com>
2014-12-06 22:05:05 -0700

    clean up of starting maze code

--- maze.js
+++ maze.js
@@ -4,142 +4,70 @@
     mazeApp.controller("MazeController", function() {
         var canvas = document.getElementById("mazecanvas");
         var context = canvas.getContext("2d");
-        var currRectX = 425;
-        var currRectY = 3;
-        var mazeWidth = 556;
-        var mazeHeight = 556;
-        var intervalVar;
+        var currRectX = 10;
+        var currRectY = 10;
 
-        drawMazeAndRectangle(425, 3);
-        window.addEventListener("keydown", moveRect, true);
-        createTimer(120);
+        var PLAYER_SIZE = 15;
 
-        function drawMazeAndRectangle(rectX, rectY) {
+        drawMaze();
+                movePlayer(0, 0, "#0000FF");
+        window.addEventListener("keydown", handleKeyPress, true);
+
+        function drawMaze() {
             makeWhite(0, 0, canvas.width, canvas.height);
-            var mazeImg = new Image();
-            mazeImg.onload = function() {
-                context.drawImage(mazeImg, 0, 0);
-                drawRectangle(rectX, rectY, "#0000FF");
-                context.beginPath();
-                context.arc(542, 122, 7, 0, 2 * Math.PI, false);
-                context.closePath();
-                context.fillStyle = '#00FF00';
-                context.fill();
-            };
-            mazeImg.src = "maze.gif";
+            context.clearRect(currRectX, currRectY, PLAYER_SIZE, PLAYER_SIZE);
+            context.beginPath();
+
+            context.rect(0, 0, canvas.width, canvas.height);
+
+            context.closePath();
+            context.fillStyle = "black";
+            context.fill();
         }
 
 
-        function drawRectangle(x, y, style) {
-            makeWhite(currRectX, currRectY, 15, 15);
-            currRectX = x;
-            currRectY = y;
+        function movePlayer(x, y, style) {
+            context.clearRect(currRectX, currRectY, PLAYER_SIZE, PLAYER_SIZE);
             context.beginPath();
-            context.rect(x, y, 15, 15);
+
+            currRectX += x;
+            currRectY += y;
+            context.rect(currRectX, currRectY, PLAYER_SIZE, PLAYER_SIZE);
+
             context.closePath();
             context.fillStyle = style;
             context.fill();
         }
 
 
-        function moveRect(e) {
-            var newX;
-            var newY;
+        function handleKeyPress(e) {
+            var newX = 0;
+            var newY = 0;
             var movingAllowed;
             e = e || window.event;
             switch (e.keyCode) {
                 case 38: // arrow up key
                 case 87: // W key
-                    newX = currRectX;
-                    newY = currRectY - 3;
+                    newY = -PLAYER_SIZE;
                     break;
                 case 37: // arrow left key
                 case 65: // A key
-                    newX = currRectX - 3;
-                    newY = currRectY;
+                    newX = -PLAYER_SIZE;
                     break;
                 case 40: // arrow down key
                 case 83: // S key
-                    newX = currRectX;
-                    newY = currRectY + 3;
+                    newY = PLAYER_SIZE;
                     break;
                 case 39: // arrow right key
                 case 68: // D key
-                    newX = currRectX + 3;
-                    newY = currRectY;
+                    newX = PLAYER_SIZE;
                     break;
             }
-            movingAllowed = canMoveTo(newX, newY);
-            if (movingAllowed === 1) { // 1 means 'the rectangle can move'
-                drawRectangle(newX, newY, "#0000FF");
-                currRectX = newX;
-                currRectY = newY;
-            } else if (movingAllowed === 2) { // 2 means 'the rectangle reached the end point'
-                clearInterval(intervalVar);
-                makeWhite(0, 0, canvas.width, canvas.height);
-                context.font = "40px Arial";
-                context.fillStyle = "blue";
-                context.textAlign = "center";
-                context.textBaseline = "middle";
-                context.fillText("Congratulations!", canvas.width / 2, canvas.height / 2);
-                window.removeEventListener("keydown", moveRect, true);
+
+            if (newX != 0 || newY != 0) {
+                movePlayer(newX, newY, "#0000FF");
+                event.preventDefault();
             }
-        }
-
-
-        function canMoveTo(destX, destY) {
-            var imgData = context.getImageData(destX, destY, 15, 15);
-            var data = imgData.data;
-            var canMove = 1; // 1 means: the rectangle can move
-            if (destX >= 0 && destX <= mazeWidth - 15 && destY >= 0 && destY <= mazeHeight - 15) {
-                for (var i = 0; i < 4 * 15 * 15; i += 4) {
-                    if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 0) { // black
-                        canMove = 0; // 0 means: the rectangle can't move
-                        break;
-                    } else if (data[i] === 0 && data[i + 1] === 255 && data[i + 2] === 0) { // #00FF00
-                        canMove = 2; // 2 means: the end point is reached
-                        break;
-                    }
-                }
-            } else {
-                canMove = 0;
-            }
-            return canMove;
-        }
-
-
-        function createTimer(seconds) {
-            intervalVar = setInterval(function() {
-                makeWhite(mazeWidth, 0, canvas.width - mazeWidth, canvas.height);
-                if (seconds === 0) {
-                    clearInterval(intervalVar);
-                    window.removeEventListener("keydown", moveRect, true);
-                    makeWhite(0, 0, canvas.width, canvas.height);
-                    context.font = "40px Arial";
-                    context.fillStyle = "red";
-                    context.textAlign = "center";
-                    context.textBaseline = "middle";
-                    context.fillText("Time's up!", canvas.width / 2, canvas.height / 2);
-                    return;
-                }
-                context.font = "20px Arial";
-                if (seconds <= 10 && seconds > 5) {
-                    context.fillStyle = "orangered";
-                } else if (seconds <= 5) {
-                    context.fillStyle = "red";
-                } else {
-                    context.fillStyle = "green";
-                }
-                context.textAlign = "center";
-                context.textBaseline = "middle";
-                var minutes = Math.floor(seconds / 60);
-                var secondsToShow = (seconds - minutes * 60).toString();
-                if (secondsToShow.length === 1) {
-                    secondsToShow = "0" + secondsToShow; // if the number of seconds is '5' for example, make sure that it is shown as '05'
-                }
-                context.fillText(minutes.toString() + ":" + secondsToShow, mazeWidth + 30, canvas.height / 2);
-                seconds--;
-            }, 1000);
         }
 
 
